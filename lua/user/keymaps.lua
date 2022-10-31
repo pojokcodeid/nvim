1ed1ef40b5c72d3e6302f122323759cc6cf0137a
asep komarudin <pojokcodeid@gmail.com>
2022-10-31 20:09:59 +0700

    coba

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -27,6 +27,7 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
