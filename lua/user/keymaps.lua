36722cbcd9c7a9a53e11630c1560dc84618878e8
asep komarudin <pojokcodeid@gmail.com>
2022-10-31 19:46:15 +0700

    percobaan

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -20,6 +20,8 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
+
+
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
