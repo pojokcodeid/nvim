2c8920996786141e6c70e70174893c5a9924b1b8
asep komarudin <pojokcodeid@gmail.com>
2022-11-28 22:00:03 +0700

    perubahan minor

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -6,7 +6,7 @@ end
 local lspconfig = require("lspconfig")
 
 -- local servers = { "jsonls", "sumneko_lua","html","cssls","tsserver"}
-local servers = { "jsonls", "sumneko_lua","html","cssls","tsserver","jdtls","pyright"}
+local servers = { "jsonls", "sumneko_lua","html","cssls","tsserver","jdtls","pyright","emmet_ls"}
 
 lsp_installer.setup({
 	ensure_installed = servers,
