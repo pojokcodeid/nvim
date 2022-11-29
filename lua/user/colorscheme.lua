6b8bb6cd5147e0715c25152a210bb8aae3400e3d
asep komarudin <pojokcodeid@gmail.com>
2022-11-29 12:15:47 +0700

    update

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,5 @@
--- local colorscheme = "tokyonight"
-local colorscheme = "onedark"
+local colorscheme = "tokyonight"
+-- local colorscheme = "onedark"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
