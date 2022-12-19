02a33be437b06b1d5bcd29daf97d91c0c8dd2d80
asep komarudin <pojokcodeid@gmail.com>
2022-12-19 07:45:20 +0700

    update

--- lua/user/lsp/config.lua
+++ lua/user/lsp/config.lua
@@ -7,7 +7,7 @@ end
 local lspconfig = require("lspconfig")
 
 -- local servers = { "jsonls", "sumneko_lua","html","cssls","tsserver"}
-local servers = {"jdtls","clangd"}
+local servers = {"jdtls"}
 
 lsp_installer.setup({
 	ensure_installed = servers,
