638ddfe38918fdddf698c3a7acf6f8b77e97a542
asep komarudin <pojokcodeid@gmail.com>
2022-12-30 20:32:54 +0700

    update icon

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -5,6 +5,8 @@ if not status_cmp_ok then
 	return
 end
 
+local icons = require("user.icons")
+
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
@@ -12,10 +14,14 @@ M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 M.setup = function()
 	local signs = {
 
-		{ name = "DiagnosticSignError", text = "" },
-		{ name = "DiagnosticSignWarn", text = "" },
-		{ name = "DiagnosticSignHint", text = "" },
-		{ name = "DiagnosticSignInfo", text = "" },
+		-- { name = "DiagnosticSignError", text = "" },
+		-- { name = "DiagnosticSignWarn", text = "" },
+		-- { name = "DiagnosticSignHint", text = "" },
+		-- { name = "DiagnosticSignInfo", text = "" },
+		{ name = "DiagnosticSignError", text = icons.diagnostics.Error },
+		{ name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
+		{ name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
+		{ name = "DiagnosticSignInfo", text = icons.diagnostics.Info },
 	}
 
 	for _, sign in ipairs(signs) do
