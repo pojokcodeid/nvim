d2085400a80a67f3a9ca277653b8cd7334d7246b
asep komarudin <pojokcodeid@gmail.com>
2022-12-30 21:07:57 +0700

    update color scheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,5 @@
--- local colorscheme = "tokyonight-night"
-local colorscheme = "lunar"
+local colorscheme = "tokyonight-night"
+-- local colorscheme = "lunar"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -120,7 +120,13 @@ return packer.startup(function(use)
 	})
 
 	-- Colorschemes
-	use({ "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" })
+	use({
+		"folke/tokyonight.nvim",
+		commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764",
+		config = function()
+			require("user.tokyonight")
+		end,
+	})
 	use({ "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" })
 
 	-- Cmp
