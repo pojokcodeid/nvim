147ba5511454d8bc7106bf7943e00b54b00ade18
asep komarudin <pojokcodeid@gmail.com>
2022-12-31 05:54:47 +0700

    ignore toggleterm

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -125,7 +125,7 @@ lualine.setup({
 		theme = "auto",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline", "DressingInput" },
+		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline", "DressingInput", "toggleterm" },
 		always_divide_middle = true,
 	},
 	sections = {
