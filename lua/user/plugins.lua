7e401b8d4f1cd64a241c619d6f09541b676d1d29
asep komarudin <pojokcodeid@gmail.com>
2023-01-03 08:16:34 +0700

    update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -233,6 +233,29 @@ return packer.startup(function(use)
 	})
 
 	use({ "shaunsingh/nord.nvim" })
+	use({
+		"petertriho/nvim-scrollbar",
+		config = function()
+			require("scrollbar").setup()
+		end,
+	})
+	use({
+		"kevinhwang91/nvim-hlslens",
+		config = function()
+			require("hlslens").setup({
+				build_position_cb = function(plist, _, _, _)
+					require("scrollbar.handlers.search").handler.show(plist.start_pos)
+				end,
+			})
+
+			vim.cmd([[
+        augroup scrollbar_search_hide
+            autocmd!
+            autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
+        augroup END
+    ]])
+		end,
+	})
 
 	-- use({ "mfussenegger/nvim-jdtls" })
 	-- use({ "ellisonleao/gruvbox.nvim" })
