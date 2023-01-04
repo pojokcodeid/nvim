81c84971bab10966e316d1ab029c5c1a9af2784e
asep komarudin <pojokcodeid@gmail.com>
2023-01-04 12:58:55 +0700

    update

--- init.lua
+++ init.lua
@@ -5,25 +5,7 @@ require("user.plugins")
 require("user.colorscheme")
 require("user.cmp")
 require("user.lsp")
--- require("user.telescope")
--- require("user.gitsigns")
--- require("user.treesitter")
--- require("user.autopairs")
--- require("user.comment")
--- require("user.nvim-tree")
--- require("user.bufferline")
--- require("user.lualine")
--- require("user.toggleterm")
--- require "user.project"  -- ini di remark dulu supaya root folder sesuai pwd
--- require("user.impatient")
--- require("user.indentline")
--- require("user.alpha")
--- require "user.whichkey"
 require("user.autocommands")
-
--- require "user.coderunner"
--- require "user.autotag"
--- require "user.colorizer"
 require("user.snip")
 require("user.webdevicons")
 require("user.breadcrumb")
