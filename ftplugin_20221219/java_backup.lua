1c5c3899d4c67c89f14075beb02f428035a7b0a6
asep komarudin <pojokcodeid@gmail.com>
2023-01-08 19:48:19 +0700

    update java config

--- ftplugin_20221219/java_backup.lua
+++ ftplugin_20221219/java_backup.lua
@@ -1,52 +1,13 @@
 -- more space in the neovim command line for displaying messages
 -- use this function notation to build some variables
-vim.opt_local.shiftwidth = 2
-vim.opt_local.tabstop = 2
-local status, jdtls = pcall(require, "jdtls")
-if not status then
-	return
-end
-
-local function capabilities()
-	local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-	if status_ok then
-		return cmp_nvim_lsp.default_capabilities()
-	end
-
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities.textDocument.completion.completionItem.snippetSupport = true
-	capabilities.textDocument.completion.completionItem.resolveSupport = {
-		properties = {
-			"documentation",
-			"detail",
-			"additionalTextEdits",
-		},
-	}
-
-	return capabilities
-end
-
-local function directory_exists(path)
-	local f = io.popen("cd " .. path)
-	local ff = f:read("*all")
-
-	if ff:find("ItemNotFoundException") then
-		return false
-	else
-		return true
-	end
-end
-
 local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
 
 -- calculate workspace dir
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = vim.fn.stdpath("data") .. "/site/java/workspace-root/" .. project_name
-if directory_exists(workspace_dir) then
-else
-	os.execute("mkdir " .. workspace_dir)
-end
+os.execute("mkdir " .. workspace_dir)
+
 -- get the mason install path
 local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
 
@@ -82,7 +43,6 @@ local config = {
 		"-data",
 		workspace_dir,
 	},
-	capabilities = capabilities(),
 	root_dir = root_dir,
 	settings = {
 		java = {},
@@ -92,4 +52,4 @@ local config = {
 		bundles = {},
 	},
 }
-jdtls.start_or_attach(config)
+require("jdtls").start_or_attach(config)
